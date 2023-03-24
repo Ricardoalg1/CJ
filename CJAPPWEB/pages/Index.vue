df0f893d78e5df1909e3d2b6ef5b4dcca4b2fb0c
Ricardoalg1 <rijaal1989g@gmail.com>
2023-03-24 17:15:46 -0500

    CJWebApp

---
+++ CJAPPWEB/pages/Index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    <h1>Inicio </h1>
+
+    <nav style="display:flex; gap: 10px;">
+      <NuxtLink to="/">inicio</NuxtLink>
+      <NuxtLink to="/monitor">monitor</NuxtLink>
+      <NuxtLink to="/asesor">asesor</NuxtLink>
+      <NuxtLink to="/estudiante">estudiante</NuxtLink>
+      
+    </nav>
+
+
+  </div>
+</template>
+
+<style scoped></style>
---
+++ CJAPPWEB/pages/login/index.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Page: login
+    
+  </div>
+</template>
+
+<style scoped></style>
