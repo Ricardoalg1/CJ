c555ce8406bf464f6e631a79f775a8058e1fde36
Ricardoalg1 <rijaal1989g@gmail.com>
2023-03-24 22:37:21 -0500

    Init

---
+++ CJAPPWEB/pages/Student/[id].vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1> {{message}}</h1>
+    </div>
+</template>
+
+<script setup>
+const route = useRoute()
+const message = computed(()=> `My Student ID is ${route.params.id}`)
+</script>
---
+++ CJAPPWEB/pages/monitor/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>
+    Monitor 
+  </div>
+</template>
+
+<style scoped></style>
